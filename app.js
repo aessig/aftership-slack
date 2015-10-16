670adff2af7643488b62232991c55a0718ed2c1a
aessig <aurelienessig@MacBook-Pro-de-Aurelien.local>
2015-10-16 19:37:33 -0300

    fix bug

--- app.js
+++ app.js
@@ -65,8 +65,5 @@ app.post('/store', function(req, res) {
 
 app.set('port', process.env.PORT || 5000));
 var server = app.listen(app.get('port'), function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  //console.log('Example app listening at http://%s:%s', host, port);
 });
